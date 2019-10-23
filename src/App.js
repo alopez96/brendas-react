dc8191a2cc2c964b267e8038598355e9465c6e75
Arturo <alopez96@ucsc.edu>
2019-10-23 09:30:47 -0700

    ismobile moved to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Hero from './components/hero/Hero';
@@ -7,9 +7,40 @@ import AboutRentals from './components/about-rentals/AboutRentals';
 import Contact from './components/contact/Contact';
 
 function App() {
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [isMobile, setisMobile] = useState(window.innerWidth < 500);
+
+  //like component did mount
+  useEffect(() => {
+    window.addEventListener('resize', () => { updateWidth() });
+
+    //returned function will be called on component unmount 
+    return () => {
+        window.removeEventListener('resize', () => {updateWidth() })
+    }
+  }, []);
+
+  //handle change in width
+  useEffect(() => {
+    handleWidthChange();
+  }, [width]);
+
+
+  
+  const updateWidth = () => {        
+    setWidth(window.innerWidth)
+  }
+
+  //method to update isMobile bool
+  const handleWidthChange = () => {
+    setisMobile( width < 500 )
+  }
+
+
   return (
     <div className='app'>
-      <AboutRentals/>
+      <AboutRentals isMobile={isMobile}/>
     </div>
   );
 }
--- src/components/about-rentals/AboutRentals.js
+++ src/components/about-rentals/AboutRentals.js
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import jumpie from './../../svg/rentals.svg';
+import React from 'react';
+import rental from './../../svg/rentals.svg';
 import './main.css';
+import tent from './../../svg/tablesandtent.svg';
 
-function AboutRentals () {
-
-    const [width, setWidth] = useState(window.innerWidth);
-    const [isMobile, setisMobile] = useState(window.innerWidth < 500);
-
-    //like component did mount
-    useEffect(() => {
-        window.addEventListener('resize', () => { updateWidth() });
-
-        //returned function will be called on component unmount 
-        return () => {
-            window.removeEventListener('resize', () => {updateWidth() })
-        }
-    }, []);
-
-    //handle change in width
-    useEffect(() => {
-        handleWidthChange();
-    }, [width]);
+function AboutRentals (props) {
 
     const header = 'Need tables, chairs, or tents?';
     const subheader = 'Tell us how many of each, and let us deliver and pickup to your location.';
@@ -29,28 +12,18 @@ function AboutRentals () {
         console.log('call clicked')
     }
 
-    const updateWidth = () => {        
-        setWidth(window.innerWidth)
-    }
-
-    //method to update isMobile bool
-    const handleWidthChange = () => {
-        setisMobile( width < 500 )
-    }
+    const isMobile = props.isMobile
 
     if(!isMobile){
         return(
-            <div>
-                <div className='main'>
+            <div className='main'>
                 <div className='content'>
-                        <h3 className='head'>{header}</h3>
-                        <p className='subhead'>{subheader}</p>
-                        
-                        <a className='primary' onClick={() => call()}>Give us a call ></a>
-                    </div>
-                    <div>
-                        <img className='image' src={jumpie}></img>
-                    </div>
+                    <h3 className='head'>{header}</h3>
+                    <p className='subhead'>{subheader}</p>    
+                    <a className='primary' onClick={() => call()}>Give us a call ></a>
+                </div>
+                <div>
+                    <img className='image' src={rental}></img>
                 </div>
             </div>
         );
@@ -58,7 +31,9 @@ function AboutRentals () {
     else{
         return(
             <div>
-                hello
+                <div className=''>
+                    <img src={tent}/>
+                </div>
             </div>
         );
     }
