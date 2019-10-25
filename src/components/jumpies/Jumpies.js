e47b6102a0cfa3758db39580094e4e8cf034514f
Arturo <alopez96@ucsc.edu>
2019-10-25 11:56:00 -0700

    jumpies layout and design complete

--- src/components/jumpies/Jumpies.js
+++ src/components/jumpies/Jumpies.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import Jumpie from './Jumpie';
+import SearchBox from './SearchBox';
 import img1 from './images/b1.jpg';
 import img2 from './images/b2.jpeg';
 
 function Jumpies() {
 
     const list = [
-        {
-            name: 'spongebob',
-            image: img1
-        },
-        {
-            name: 'junior',
-            image: img2
-        }
+        { name: 'spongebob', image: img1 },
+        { name: 'junior', image: img2 },
+        { name: 'spongebob', image: img1 },
+        { name: 'junior', image: img2 },
+        { name: 'spongebob', image: img1 },
+        { name: 'junior', image: img2 },
+        { name: 'spongebob', image: img1 },
+        { name: 'junior', image: img2 },
+        { name: 'spongebob', image: img1 },
+        { name: 'junior', image: img2 },
     ]
     
     return(
         <div className='background'>
-        <div className='items'>
-            {list.map((item,index) => {
-                return(
-                    <Jumpie pic={item.image} key={index} title={item.name}/>
-                )
-            })}
-        </div>
+            <SearchBox/>    
+            <div className='list'>
+                {list.map((item,index) => {
+                    return(
+                        <Jumpie pic={item.image} key={index} title={item.name}/>
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/components/jumpies/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './jumpies.css';
+
+const SearchBox = ({ search }) => {
+
+    return(
+        <div className='search-container'>
+            <label>Find a jump house.</label>
+            <input className='search-box'
+                 placeholder='Enter a name of a character.' onChange={search}/>
+        </div>
+    )
+}
+
+export default SearchBox;
