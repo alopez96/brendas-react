5306015359fb087639c867b55b83586745554870
Arturo <alopez96@ucsc.edu>
2019-10-28 23:06:02 -0700

    images added

--- src/components/jumpies/Jumpies.js
+++ src/components/jumpies/Jumpies.js
@@ -3,6 +3,15 @@ import Jumpie from './Jumpie';
 import SearchBox from './SearchBox';
 import img1 from './images/b1.jpg';
 import img2 from './images/b2.jpeg';
+import img3 from './images/b3.jpg';
+import img4 from './images/b4.jpg';
+import img5 from './images/b5.jpg';
+import img6 from './images/b6.jpg';
+import img7 from './images/b7.jpg';
+import img8 from './images/b8.jpg';
+import img9 from './images/b9.jpeg';
+import tent from './images/tent.jpg';
+
 
 function Jumpies({ updateRoute }) {
 
@@ -11,16 +20,18 @@ function Jumpies({ updateRoute }) {
     const backBtn = '< Go back';
 
     const list = [
-        { name: 'spongebob', image: img1, },
-        { name: 'junior', image: img2 },
-        { name: 'spongebob', image: img1 },
-        { name: 'junior', image: img2 },
-        { name: 'spongebob', image: img1 },
-        { name: 'junior', image: img2 },
-        { name: 'spongebob', image: img1 },
-        { name: 'junior', image: img2 },
-        { name: 'spongebob', image: img1 },
-        { name: 'junior', image: img2 },
+        
+        { name: 'slide', image: img2 },
+        
+        { name: 'slide 2', image: img4 },
+        { name: 'slide 3', image: img9 },
+        { name: 'pink slide', image: img8 },
+        { name: 'Pony', image: img1, },
+        { name: 'Moana', image: img3 },
+        { name: 'Batman', image: img5 },
+        { name: 'The Good Dinosaur', image: img6 },
+        { name: 'Mikey Mouse', image: img7 },
+        { name: 'tent', image: tent },
     ]
 
     const updateSearchfield = (event) => {
