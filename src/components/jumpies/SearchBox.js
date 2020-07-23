36d55ecc2f701b1ce7b1eb22f0c3525192f035d3
Arturo Lopez <alopez96@ucsc.edu>
2020-07-23 09:15:12 -0700

    search box styled

--- src/components/jumpies/SearchBox.js
+++ src/components/jumpies/SearchBox.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import './jumpies.css';
 
-const SearchBox = ({ search, isMobile }) => {
+const SearchBox = ({ search }) => {
 
-    if(!isMobile){
-        return(
-            <div className='search-container'>
-                <label>Find a jump house</label>
-                <input className='search-box' type='search'
-                    placeholder='Enter a name' onChange={search}/>
-            </div>
-        )}
-    else{
-        return(
-            <div className='search-container-m'>
-                <label>Find a jump house</label>
-                <input className='search-box-m' type='search'
-                    placeholder='Enter a name' onChange={search}/>
-            </div>
-        )
-    }
+    return(
+        <div className='search-container'>
+            <label>Find a jump house</label>
+            <input className='search-box' type='search'
+                placeholder='Enter a name' onChange={search}/>
+        </div>
+    )
 }
 
 export default SearchBox;
